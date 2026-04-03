c617308e7bcb5433687b64fcf0e45d50dc1737da
chetan <156156147+chetan3622@users.noreply.github.com>
2026-04-03 12:04:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ col1, col2 = st.columns([1,1])
 
 # ---------------- LEFT CARD ----------------
 with col1:
-    st.markdown()
+    st.markdown('<div class="card">', unsafe_allow_html=True)
     st.subheader("📤 Upload Resume")
     uploaded_file = st.file_uploader("", type=["txt"])
     st.markdown("</div>", unsafe_allow_html=True)
