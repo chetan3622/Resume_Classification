65cc434a69d17f99f61411b043517332223819f5
chetan <156156147+chetan3622@users.noreply.github.com>
2026-05-09 21:52:33 +0530

    Update subheader text for resume upload section

--- app.py
+++ app.py
@@ -209,7 +209,7 @@ col1, col2 = st.columns(2)
 
 # ---------------- LEFT SECTION ----------------
 with col1:
-    st.subheader("Upload Resume")
+    st.subheader("Upload  Your Resume")
     with st.container(border=True):
         uploaded_file = st.file_uploader("Upload your resume", type=["txt", "pdf", "docx"])
 
