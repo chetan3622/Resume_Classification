2a40f3c9b73442f526d8f498cc7b17fb8cd0fcf5
chetan <156156147+chetan3622@users.noreply.github.com>
2026-07-24 00:03:01 +0530

    Remove extra newline in app.py

--- app.py
+++ app.py
@@ -194,7 +194,6 @@ textarea {
 
 st.markdown('<div class="title"> RESUME CLASSIFIER</div>', unsafe_allow_html=True)
 st.markdown('<div class="subtitle">Analyze your resume with Machine Learning, NLP, and smart skill matching</div>', unsafe_allow_html=True)
-
 # ----------------
 col1, col2 = st.columns(2)
 
